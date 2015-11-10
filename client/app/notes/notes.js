3cec6b0017f41d48b8a3990c134319ad88c7ec1d
Clayton Molnar <molnarclay@gmail.com>
2015-11-10 18:20:48 -0500

    write service

--- client/app/notes/notes.js
+++ client/app/notes/notes.js
@@ -1,42 +1,46 @@
-/**
- * Created by claytonmolnar on 11/9/15.
- */
 (function() {
   angular.module('notely.notes', [
-      'ui.router'
-    ])
-    .config(notesConfig);
+    'ui.router'
+  ])
+  .config(notesConfig);
 
   notesConfig['$inject'] = ['$stateProvider'];
-
   function notesConfig($stateProvider) {
     $stateProvider
 
       .state('notes', {
-      url: '/notes',
-      templateUrl: '/notes/notes.html',
-      controller: NotesController
-    })
+        url: '/notes',
+        templateUrl: '/notes/notes.html',
+        controller: NotesController
+      })
 
-    .state('notes.form', {
-      url: '/:noteId',
-      templateUrl: '/notes/notes-form.html'
-
-    });
+      .state('notes.form', {
+        url: '/:noteId',
+        templateUrl: '/notes/notes-form.html'
+      });
   }
 
   NotesController['$inject'] = ['$state', '$scope', 'NotesService'];
-
   function NotesController($state, $scope, NotesService) {
-    NotesService.fetch(function(notesData) {
-      $scope.notes = notesData;
-    });
+    $scope.note = {};
 
     $scope.save = function() {
-      console.log($scope.note.title);
       NotesService.save($scope.note);
     };
 
+    NotesService.fetch().then(function() {
+      $scope.notes = NotesService.get();
+    });
     $state.go('notes.form');
   }
 })();
+
+
+
+
+
+
+
+
+
+//
--- client/app/services/notes-service.js
+++ client/app/services/notes-service.js
@@ -1,34 +1,51 @@
 angular.module('notely')
   .service('NotesService', NotesService);
 
+// NotesService
+// Handle CRUD operations against the server.
 NotesService.$inject = ['$http'];
-
 function NotesService($http) {
   var self = this;
   self.notes = [];
 
-  self.fetch = function(callback) {
-    $http.get('http://localhost:3000/notes')
-      .success(function(notesData) {
-        console.log(notesData)
-          //  debuggerBb;
-        self.notes = notesData;
-        if (callback) {
-          callback(self.notes);
-        }
-      });
+  // Get all notes from server
+  self.fetch = function() {
+    return $http.get('http://localhost:3000/notes')
+    .then(
+      // Success callback
+      function(response) {
+        self.notes = response.data;
+      },
+      // Failure callback
+      function(response) {
+        // TODO: Handle failure
+      }
+    );
   };
 
   self.get = function() {
-    return self
+    return self.notes;
   };
 
   self.save = function(note) {
-    $http.post('http://localhost:3000/notes',{
-      note: note}).then(function(response){
-      self.notes.unshift  (response.data.note) ;
-      });
-
-  };
-
+    $http.post('http://localhost:3000/notes', {
+      note: note
+    }).then(function(response) {
+      self.notes.unshift(response.data.note);
+    });
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
